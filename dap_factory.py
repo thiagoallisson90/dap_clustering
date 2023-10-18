1611e74dfb529ce71d6c7019c34958ef80ab6d9c
Thiago Allisson <thiago.allisson@ufpi.edu.br>
2023-10-18 15:01:10 -0300

    Adjusts in the dap_factory file

--- dap_factory.py
+++ dap_factory.py
@@ -1,11 +1,4 @@
 import numpy as np
-from sklearn.cluster import KMeans
-from sklearn_extra.cluster import KMedoids
-from fcluster import FCluster
-from skfuzzy.cluster import cmeans
-from sklearn.metrics import pairwise_distances
-
-from dap_utils import generate_ed_coords
 
 ##############################
 # Clustering Model Factories #
@@ -28,6 +21,7 @@ class Factory:
 
 class KMeansFactory(Factory):
   def __init__(self, n_clusters=2):
+    from sklearn.cluster import KMeans
     super().__init__(n_clusters)
     self.clf = KMeans(n_clusters, n_init='auto', init='k-means++')
   
@@ -38,6 +32,7 @@ class KMeansFactory(Factory):
   
 class KMedoidsFactory(Factory):
   def __init__(self, n_clusters=2):
+    from sklearn_extra.cluster import KMedoids
     super().__init__(n_clusters)
     self.clf = KMedoids(n_clusters)
   
@@ -51,12 +46,14 @@ class CMeansSKFactory(Factory):
     super().__init__(n_clusters)
 
   def fit(self, X):
+    from skfuzzy.cluster import cmeans
     self.cluster_centers, u, _, _, _, _, _ = \
       cmeans(X.T, c=self.n_clusters, m=2, error=0.005, maxiter=1000)
     self.labels = np.argmax(u, axis=0)    
     
 class CMeansFCFactory(Factory):
   def __init__(self, n_clusters=2):
+    from fcluster import FCluster
     super().__init__(n_clusters)
     self.clf = FCluster(n_clusters, fuzzines=2, error=0.005, max_iter=1000)
 
@@ -66,6 +63,7 @@ class CMeansFCFactory(Factory):
   
 class GKFactory(Factory):
   def __init__(self, n_clusters=2):
+    from fcluster import FCluster
     super().__init__(n_clusters)
     self.clf = \
       FCluster(n_clusters, fuzzines=2, error=0.005, max_iter=1000, method='Gustafson–Kessel')
@@ -79,6 +77,9 @@ class RandFactory(Factory):
     super().__init__(n_clusters)
   
   def fit(self, X):
+    from sklearn.metrics import pairwise_distances
+    from dap_utils import generate_ed_coords
+
     self.cluster_centers = generate_ed_coords(self.n_clusters, seed=None)
     dists = pairwise_distances(X, self.cluster_centers, n_jobs=-1)
     self.labels = [np.argmin(dist) for dist in dists]
