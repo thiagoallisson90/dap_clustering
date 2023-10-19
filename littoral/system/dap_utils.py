fb22acf29f8c05371c2fb0ba54a2331f5b0df3bd
Thiago Allisson <thiago.allisson@ufpi.edu.br>
2023-10-19 09:17:29 -0300

    Fixing dap_utils and improving test.py

--- littoral/system/dap_utils.py
+++ littoral/system/dap_utils.py
@@ -3,22 +3,7 @@ import os
 import matplotlib.pyplot as plt
 import pandas as pd
 
-scratch_dir = '/home/thiago/Documentos/Doutorado/Simuladores/ns-3-dev/scratch'
-base_dir = '/home/thiago/Documentos/Doutorado/Simuladores/ns-3-dev/scratch/dap_clustering'
-img_dir = '/home/thiago/Documentos/Doutorado/Simuladores/ns-3-dev/scratch/dap_clustering/imgs'
-data_dir = '/home/thiago/Documentos/Doutorado/Simuladores/ns-3-dev/scratch/dap_clustering/data'
-ed_pos_file = 'ed_pos_file.csv'
-ed_out_file = 'ed_out_file.csv'
-gw_pos_file = 'gw_pos_file.csv'
-ns3_cmd = '/home/thiago/Documentos/Doutorado/Simuladores/ns-3-dev/./ns3'
-clustering_models = ['kmeans', 'kmedoids', 'cmeans', 'gk', 'rand', 'tests']
-model_names = {
-  'kmeans': 'K-Means',
-  'kmedoids': 'K-Medoids',
-  'cmeans':  'Fuzzy C-Means',
-  'gk': 'Gustafson-Kessel',
-  'rand': 'Rand'
-}
+from littoral.system.dap_vars import *
 
 #####################
 # Utility Functions #
--- test.py
+++ test.py
@@ -1,5 +1,6 @@
 from littoral.system.dap_utils import generate_ed_coords
-from littoral.system.dap_factory import KMeansFactory, KMedoidsFactory, CMeansSKFactory, GKFactory, RandFactory
+from littoral.system.dap_factory import KMeansFactory, KMedoidsFactory, CMeansSKFactory
+from littoral.system.dap_factory import GKFactory, RandFactory
 from littoral.system.dap_plot import plot_clusters, plot_copex, plot_dists
 
 if __name__ == '__main__':
@@ -7,31 +8,15 @@ if __name__ == '__main__':
 
   ks = [17, 16, 16, 16, 16, 25]
 
-  clf1 = KMeansFactory(ks[0])
-  clf1.fit(coords)
-
-  clf2 = KMedoidsFactory(ks[1])
-  clf2.fit(coords)
-
-  clf3 = CMeansSKFactory(ks[2])
-  clf3.fit(coords)
-
-  clf4 = GKFactory(ks[3])
-  clf4.fit(coords)
-
-  clf5 = RandFactory(ks[4])
-  clf5.fit(coords)
-
-  clf6 = RandFactory(ks[5])
-  clf6.fit(coords)
-
-  clfs = [clf1, clf2, clf3, clf4, clf5, clf6]
+  clfs = [KMeansFactory(ks[0]), KMedoidsFactory(ks[1]), CMeansSKFactory(ks[2]), GKFactory(ks[3]), 
+          RandFactory(ks[4]), RandFactory(ks[5])]
   folders = ['kmeans', 'kmedoids', 'cmeans', 'gk', 'rand', 'rand']
   names = []
   text = ''
   mean_dist, max_dist = [], []
 
   for i in range(len(clfs)):
+    clfs[i].fit(coords)
     name = clfs[i].name
     names.append(name)
     
@@ -45,7 +30,7 @@ if __name__ == '__main__':
     elif(i < len(clfs) - 1):
         text = text + ', ' + name
     else:
-        text = text + ' and' + name
+        text = text + ' and ' + name
 
   plot_copex(ks, labels=names, capex_text=f'CapEx - {text}', opex_text=f'OpEx - {text}')
   plot_dists(ks, labels=names, mean_dist=mean_dist, max_dist=max_dist)
