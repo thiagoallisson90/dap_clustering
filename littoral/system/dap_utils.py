94d25a8c7883feadcd806df666018ad5eca0dd98
Thiago Allisson <thiago.allisson@ufpi.edu.br>
2023-10-24 13:40:45 -0300

    Fixing normal_test

--- littoral/system/dap_utils.py
+++ littoral/system/dap_utils.py
@@ -52,7 +52,7 @@ def compute_consumed_energy(ks, folders, data_dir=data_dir, n_reps=30):
         k = ks[j]
         folder = folders[j]
         dfs = \
-            [pd.read_csv(f'{data_dir}/{folder}/nRun_{i}_{k}gws_battery-level.txt', names=col_names).drop(0, axis=0) \
+            [pd.read_csv(f'{data_dir}/{folder}/nRun_{i}_{k}gws_battery-level.txt', names=col_names) \
                 for i in range(1, n_reps+1)]
         
         energy_values[j] = [initial_energy - df['remainder_energy'].mean() for df in dfs]
@@ -63,7 +63,6 @@ def normal_test(data):
     alpha = 0.05
     p = 0.0
     size = len(data)
-    indexes = data.sort_values().index
 
     if(size > 4 and size <= 30):
         from scipy.stats import shapiro        
@@ -71,54 +70,28 @@ def normal_test(data):
     elif(size > 30 and size <= 50):
         from statsmodels.stats.diagnostic import lilliefors
         _, p = lilliefors(data, dist='norm', pvalmethod='approx')
-
-    aux = 0
-    median_indexes = []
-    if(size % 2 == 0):
-        aux = int(size / 2.0)
-        median_indexes.append(indexes[int(aux)-1])
-        median_indexes.append(indexes[int(aux)])
-    else:
-        aux = int((size+1)/2.0)
-        median_indexes.append(indexes[aux])
     
     if(p >= alpha):
-        return True, p, median_indexes
+        return True, p
     
-    return False, p, median_indexes
+    return False, p
 
 def full_normal_test(k, folder):
     file = f'{data_dir}/{folder}/tracker_unconfirmed_buildings{k}gw.csv'
     names=['Sent', 'Received', 'UL-PDR', 'RSSI', 'SNR', 'Delay']
     df = pd.read_csv(file, names=names)
     ps = {}
-    index_df = {}
 
     for name in names:
         if(name not in ['Sent', 'Received']):
             data = df[name]
-            result, p, median_indexes = normal_test(data)
+            _, p = normal_test(data)
             ps[name.lower()] = [p]
-            if(result):
-                if(len(median_indexes) == 2):
-                    first, second = median_indexes
-                    index_df[name.lower()] = (first+1, second+1)
-                elif(len(median_indexes) == 1):
-                    index_df[name.lower()] = median_indexes
 
     energy = compute_consumed_energy(ks=[k], folders=[folder])
     energy_serie = pd.Series(energy[0])
-    result, p, median_indexes = normal_test(energy_serie)
+    _, p = normal_test(energy_serie)
     ps['energy'] = p
 
-    if(result):
-        if(len(median_indexes) == 2):
-            first, second = median_indexes
-            index_df['energy'] = (first+1, second+1)
-        elif(len(median_indexes) == 1):
-            index_df['energy'] = median_indexes
-
     pd.DataFrame(ps).to_csv(f'{data_dir}/{folder}/p_values.csv', index=False)
-    pd.DataFrame(index_df).to_csv(f'{data_dir}/{folder}/median_indexes.csv', index=False)
-
 #####################
