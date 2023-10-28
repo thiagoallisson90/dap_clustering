05c44f9544922fcfb9d54052b56f3f380a9dbb69
Thiago Allisson <thiago.allisson@ufpi.edu.br>
2023-10-28 03:55:25 -0300

    Adding treatment to sf file

--- littoral/system/dap_simulate.py
+++ littoral/system/dap_simulate.py
@@ -6,21 +6,27 @@ from littoral.system.dap_vars import ed_pos_file, ed_out_file, gw_pos_file
 from littoral.system.dap_vars import data_dir, ns3_cmd
 
 def simulate(coords, centroids, folder, ed_pos_file=ed_pos_file, ed_out_file=ed_out_file, 
-             gw_pos_file=gw_pos_file, radius=10000, load=5, useConnFile=0, connFile=''):
+             gw_pos_file=gw_pos_file, radius=10000, load=5, setUpSF=0, connFile=''):
     script='scratch/dap_clustering/dap_clustering.cc'
     n_gw = len(centroids)
     n_simulatons = 30
 
     write_coords(coords, ed_pos_file)
     write_coords(centroids, gw_pos_file)
+    
     filename = f'{data_dir}/{folder}/tracker_unconfirmed_buildings{n_gw}gw.csv'
+    with open(filename, mode="w") as file:
+        file.write('')
+        file.close()
+    
+    filename = f'{data_dir}/{folder}/{n_gw}gw_sf.csv'
     with open(filename, mode="w") as file:
         file.write('')
         file.close()
 
     params01 = f'--edPos={ed_pos_file} --edOutputFile={ed_out_file} --gwPos={gw_pos_file} --nGateways={n_gw}'
     params02 = f'--cModel={folder} --radius={radius} --nDevices={len(coords)} --lambda={load}'
-    params03 = f'--useConnFile={useConnFile} --connFile={connFile}'
+    params03 = f'--setUpSF={setUpSF} --connFile={connFile}'
 
     for i in range(1, n_simulatons+1):
         run_cmd = f'{ns3_cmd} run "{script} {params01} {params02} {params03} --nRun={i}"'
